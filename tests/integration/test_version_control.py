7c287a129cd5d13cc62061c1e00b9ef2937da745
Asher Norland <asher.norland@gmail.com>
2023-06-02 16:03:23 -0400

    Update test_version_control.py

--- tests/integration/test_version_control.py
+++ tests/integration/test_version_control.py
@@ -6,6 +6,7 @@ from pytest_cppython.tests import SCMIntegrationTests
 
 from cppython_git.plugin import GitSCM
 
+
 class TestGitInterface(SCMIntegrationTests[GitSCM]):
     """Integration tests for the Git SCM plugin"""
 
